218c9e8893d02cc4be125f08883106d30fd8b868
dev126712 <solonoobhacker6767@gmail.com>
2025-11-28 11:35:12 -0500

    jwt auth

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({ title: 'GET all uses' }));
+userRouter.get('/', (req, res) => res.send({ title: 'GET all users' }));
 
 userRouter.get('/:id', (req, res) => res.send({ title: 'GET user detail' }));
 
