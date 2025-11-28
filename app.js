6c67c1b186aec2d8c48d52b6ae23aa30a2db0d41
dev126712 <solonoobhacker6767@gmail.com>
2025-11-28 09:07:40 -0500

    frist commit

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { PORT } from './config/env.js'
+
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import connectDB from './database/mongodb.js';
+
+const app = express();
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscription', subscriptionRouter);
+
+app.get('/', (req, res) => {
+    res.send('welcome to sub tracker api');
+});
+
+app.listen( PORT, async () => {
+    console.log(`server running on http://localhost:${PORT}`);
+    await connectDB();
+});
+
+export default app;
---
+++ database/mongodb.js
@@ -0,0 +1,18 @@
+import mongose from 'mongoose';
+import { DB_URI, NODE_ENV } from '../config/env.js';
+
+if(!DB_URI){
+    throw new Error('please define the MONGO_URI environment variable inside .env.<development/production>.local file');
+}
+
+const connectDB = async () => {
+    try {
+        await mongose.connect(DB_URI);
+        console.log(`MongoDB connected successfully in ${NODE_ENV} mode`);
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
