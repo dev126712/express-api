a2837b063b36545ecc6146ca85c11e7e7410ac80
dev126712 <solonoobhacker6767@gmail.com>
2025-11-28 12:13:49 -0500

    auth middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { getUsers, getUser } from "../controllers/user.controller.js";
+import  authorize  from "../middleware/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({ title: 'GET all users' }));
+userRouter.get('/', getUsers);
 
-userRouter.get('/:id', (req, res) => res.send({ title: 'GET user detail' }));
+userRouter.get('/:id', authorize, getUser);
 
 userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
 
